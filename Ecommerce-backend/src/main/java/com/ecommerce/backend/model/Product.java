35c63f34e8da3c36c864331660d215a9bf9e2b54
sissy5656 <supersissy5656@gmail.com>
2020-09-15 11:14:40 -0500

    Change product model

--- Ecommerce-backend/src/main/java/com/ecommerce/backend/model/Product.java
+++ Ecommerce-backend/src/main/java/com/ecommerce/backend/model/Product.java
@@ -39,106 +39,89 @@ public class Product {
     @NotBlank
     private ArrayList<String> color;
     
+    @NotBlank
+    private String description;
+    
     public Product() {
     	
     }
 
 	public Product(@NotBlank @Size(max = 200) String name, @NotBlank String image,
 			@NotBlank @Size(max = 20) String type, @NotBlank float price, @NotBlank ArrayList<String> size,
-			@NotBlank ArrayList<String> color) {
+			@NotBlank ArrayList<String> color, @NotBlank String description) {
 		this.name = name;
 		this.image = image;
 		this.type = type;
 		this.price = price;
 		this.size = size;
 		this.color = color;
+		this.description = description;
 	}
-
-
 
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getImage() {
 		return image;
 	}
 
-
-
 	public void setImage(String image) {
 		this.image = image;
 	}
-
-
 
 	public String getType() {
 		return type;
 	}
 
-
-
 	public void setType(String type) {
 		this.type = type;
 	}
-
-
 
 	public float getPrice() {
 		return price;
 	}
 
-
-
 	public void setPrice(float price) {
 		this.price = price;
 	}
-
-
 
 	public ArrayList<String> getSize() {
 		return size;
 	}
 
-
-
 	public void setSize(ArrayList<String> size) {
 		this.size = size;
 	}
-
-
 
 	public ArrayList<String> getColor() {
 		return color;
 	}
 
-
-
 	public void setColor(ArrayList<String> color) {
 		this.color = color;
 	}
-    
-    
-   
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
 
 }
