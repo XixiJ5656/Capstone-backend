125fadb9596a49462739493673e1b7d716abe993
sissy5656 <supersissy5656@gmail.com>
2020-09-23 19:02:25 -0500

    Modified

--- Ecommerce-backend/src/main/java/com/ecommerce/backend/controller/UserController.java
+++ Ecommerce-backend/src/main/java/com/ecommerce/backend/controller/UserController.java
@@ -2,11 +2,12 @@ package com.ecommerce.backend.controller;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ import com.ecommerce.backend.repository.UserRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/app")
+@RequestMapping("/api/auth")
 public class UserController {
 
 	
@@ -28,5 +29,25 @@ public class UserController {
 	    public List<User> getAllUsers(Model model) {
 	        return this.userRepository.findAll();
 	    }  	
+	    
+	    
+	    
+	    // DELETE ALL USERS
+	       
+	    @DeleteMapping("/users")
+	    public void deleteUsers() {
+	        userRepository.deleteAll();
+	    }
+	       
+	       
+	   
+	    // DELETE USER BY ID
+	    
+	    @DeleteMapping("/users/{id}")
+	    public void deleteUserById(@PathVariable Long id) {
+	        userRepository.deleteById(id);
+	    }
+	    
+	    
 	    
 }
